e2f3434d58515305ef827636f063d08b3e655423
oliver.schwarz <oliver.schwarz@af6d066f-262b-0410-944b-b18571a5ebb2>
2009-09-10 09:50:06 +0000

    Adding several issue solutions (as prototypes): * Issue 8: Numbers are now delivered as numbers (this is configurable) * Issue 9: Using a simple encoding for strings in fields (this must be improved still)

--- branches/version02/php-excel.class.php
+++ branches/version02/php-excel.class.php
@@ -52,6 +52,7 @@
  * @todo Add configuration to activate/deactivate the string conversion
  * @todo Add option to give out first line as header (bold text)
  * @todo Add option to give out last line as footer (bold text)
+ * @todo Add option to write to file
  */
 class Excel_XML
 {
@@ -61,11 +62,7 @@ class Excel_XML
      * @access private
      * @var string
      */
-    private $header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?\>
-<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\"
- xmlns:x=\"urn:schemas-microsoft-com:office:excel\"
- xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\"
- xmlns:html=\"http://www.w3.org/TR/REC-html40\">";
+    private $header = "<?xml version=\"1.0\" encoding=\"%s\"?\>\n<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:html=\"http://www.w3.org/TR/REC-html40\">";
 
     /**
      * Footer of excel document (appended to the rows)
@@ -90,6 +87,12 @@ class Excel_XML
         private $sEncoding;
         
         /**
+         * Convert variable types
+         * @var boolean
+         */
+        private $bConvertTypes;
+        
+        /**
          * Worksheet title
          * @var string
          */
@@ -102,37 +105,53 @@ class Excel_XML
          * parameters so that the library may be configured to
          * one's needs.
          * 
+         * On converting types:
+         * When set to true, the library tries to identify the type of
+         * the variable value and set the field specification for Excel
+         * accordingly. Be careful with article numbers or postcodes
+         * starting with a '0' (zero)!
+         * 
          * @param string $sEncoding Encoding to be used (defaults to UTF-8)
+         * @param boolean $bConvertTypes Convert variables to field specification
          */
-        public function __construct($sEncoding = 'UTF-8')
+        public function __construct($sEncoding = 'UTF-8', $bConvertTypes = false, $sWorksheetTitle = 'Table1')
+        {
+                $this->bConvertTypes = $bConvertTypes;
+        	$this->setEncoding($sEncoding);
+        	$this->setWorksheetTitle($sWorksheetTitle);
+        }
+        
+        /**
+         * Set encoding
+         * @param string Encoding type to set
+         */
+        private function setEncoding($sEncoding)
         {
         	$this->sEncoding = $sEncoding;
+        } 
+
+        /**
+         * Add row
+         * 
+         * Adds a single row to the document. If set to true, self::bConvertTypes
+         * checks the type of variable and returns the specific field settings
+         * for the cell.
+         * 
+         * @param array $array One-dimensional array with row content
+         */
+        private function addRow ($array)
+        {
+        	$cells = "";
+                foreach ($array as $k => $v):
+                        $type = 'String';
+                        if ($this->bConvertTypes === true && is_numeric($v)):
+                                $type = 'Number';
+                        endif;
+                        $v = htmlentities($v, ENT_COMPAT, $this->sEncoding);
+                        $cells .= "<Cell><Data ss:Type=\"$type\">" . $v . "</Data></Cell>\n"; 
+                endforeach;
+                $this->lines[] = "<Row>\n" . $cells . "</Row>\n";
         }
-    
-    /**
-     * Add a single row to the $document string
-     * 
-     * @access private
-     * @param array 1-dimensional array
-     * @todo Row-creation should be done by $this->addArray
-     */
-    private function addRow ($array)
-    {
-
-        // initialize all cells for this row
-        $cells = "";
-
-        // foreach key -> write value into cells
-        foreach ($array as $k => $v):
-
-            $cells .= "<Cell><Data ss:Type=\"String\">" . $v . "</Data></Cell>\n"; 
-
-        endforeach;
-
-        // transform $cells content into one row
-        $this->lines[] = "<Row>\n" . $cells . "</Row>\n";
-
-    }
 
     /**
      * Add an array to the document
@@ -190,16 +209,15 @@ class Excel_XML
     {
 
         // deliver header (as recommended in php manual)
-        header("Content-Type: application/vnd.ms-excel; charset=UTF-8");
+        header("Content-Type: application/vnd.ms-excel; charset=" . $this->sEncoding);
         header("Content-Disposition: inline; filename=\"" . $filename . ".xls\"");
 
         // print out document to the browser
         // need to use stripslashes for the damn ">"
-        echo stripslashes ($this->header);
+        echo stripslashes (sprintf($this->header, $this->sEncoding));
         echo "\n<Worksheet ss:Name=\"" . $this->sWorksheetTitle . "\">\n<Table>\n";
-        echo "<Column ss:Index=\"1\" ss:AutoFitWidth=\"0\" />\n";
         foreach ($this->lines as $line):
-                echo $line . "\n";
+                echo $line;
         endforeach;
         echo "</Table>\n</Worksheet>\n";
         echo $this->footer;
