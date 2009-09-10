4adc2ca1325fd7d51e57fdd7b9ca2bc0043d2f23
oliver.schwarz <oliver.schwarz@af6d066f-262b-0410-944b-b18571a5ebb2>
2009-09-10 14:34:54 +0000

    Adding the new version 1.1 (and new testcases, single-file license and readme) to the trunk. Old version is tagged 01 and is still available.

--- trunk/php-excel.class.php
+++ trunk/php-excel.class.php
@@ -2,35 +2,9 @@
 
 /**
  * Simple excel generating from PHP5
- * 
- * This is one of my utility-classes.
- * 
- * The MIT License
- * 
- * Copyright (c) 2007 Oliver Schwarz
- * 
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
  *
  * @package Utilities
+ * @license http://www.opensource.org/licenses/mit-license.php
  * @author Oliver Schwarz <oliver.schwarz@gmail.com>
  * @version 1.0
  */
@@ -44,152 +18,157 @@
  * @package Utilities
  * @subpackage Excel
  * @author Oliver Schwarz <oliver.schwarz@vaicon.de>
- * @version 1.0
- *
-  * @todo Add error handling (array corruption etc.)
- * @todo Write a wrapper method to do everything on-the-fly
+ * @version 1.1
+ * 
+ * @todo Issue #4: Internet Explorer 7 does not work well with the given header
+ * @todo Add option to give out first line as header (bold text)
+ * @todo Add option to give out last line as footer (bold text)
+ * @todo Add option to write to file
  */
 class Excel_XML
 {
 
-    /**
-     * Header of excel document (prepended to the rows)
-     * 
-     * Copied from the excel xml-specs.
-     * 
-     * @access private
-     * @var string
-     */
-    private $header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?\>
-<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\"
- xmlns:x=\"urn:schemas-microsoft-com:office:excel\"
- xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\"
- xmlns:html=\"http://www.w3.org/TR/REC-html40\">";
+	/**
+	 * Header (of document)
+	 * @var string
+	 */
+        private $header = "<?xml version=\"1.0\" encoding=\"%s\"?\>\n<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:html=\"http://www.w3.org/TR/REC-html40\">";
 
-    /**
-     * Footer of excel document (appended to the rows)
-     * 
-     * Copied from the excel xml-specs.
-     * 
-     * @access private
-     * @var string
-     */
-    private $footer = "</Workbook>";
+        /**
+         * Footer (of document)
+         * @var string
+         */
+        private $footer = "</Workbook>";
 
-    /**
-     * Document lines (rows in an array)
-     * 
-     * @access private
-     * @var array
-     */
-    private $lines = array ();
+        /**
+         * Lines to output in the excel document
+         * @var array
+         */
+        private $lines = array();
 
-    /**
-     * Worksheet title
-     *
-     * Contains the title of a single worksheet
-     *
-     * @access private 
-     * @var string
-     */
-    private $worksheet_title = "Table1";
+        /**
+         * Used encoding
+         * @var string
+         */
+        private $sEncoding;
+        
+        /**
+         * Convert variable types
+         * @var boolean
+         */
+        private $bConvertTypes;
+        
+        /**
+         * Worksheet title
+         * @var string
+         */
+        private $sWorksheetTitle;
 
-    /**
-     * Add a single row to the $document string
-     * 
-     * @access private
-     * @param array 1-dimensional array
-     * @todo Row-creation should be done by $this->addArray
-     */
-    private function addRow ($array)
-    {
+        /**
+         * Constructor
+         * 
+         * The constructor allows the setting of some additional
+         * parameters so that the library may be configured to
+         * one's needs.
+         * 
+         * On converting types:
+         * When set to true, the library tries to identify the type of
+         * the variable value and set the field specification for Excel
+         * accordingly. Be careful with article numbers or postcodes
+         * starting with a '0' (zero)!
+         * 
+         * @param string $sEncoding Encoding to be used (defaults to UTF-8)
+         * @param boolean $bConvertTypes Convert variables to field specification
+         * @param string $sWorksheetTitle Title for the worksheet
+         */
+        public function __construct($sEncoding = 'UTF-8', $bConvertTypes = false, $sWorksheetTitle = 'Table1')
+        {
+                $this->bConvertTypes = $bConvertTypes;
+        	$this->setEncoding($sEncoding);
+        	$this->setWorksheetTitle($sWorksheetTitle);
+        }
+        
+        /**
+         * Set encoding
+         * @param string Encoding type to set
+         */
+        public function setEncoding($sEncoding)
+        {
+        	$this->sEncoding = $sEncoding;
+        }
 
-        // initialize all cells for this row
-        $cells = "";
+        /**
+         * Set worksheet title
+         * 
+         * Strips out not allowed characters and trims the
+         * title to a maximum length of 31.
+         * 
+         * @param string $title Title for worksheet
+         */
+        public function setWorksheetTitle ($title)
+        {
+                $title = preg_replace ("/[\\\|:|\/|\?|\*|\[|\]]/", "", $title);
+                $title = substr ($title, 0, 31);
+                $this->sWorksheetTitle = $title;
+        }
 
-        // foreach key -> write value into cells
-        foreach ($array as $k => $v):
+        /**
+         * Add row
+         * 
+         * Adds a single row to the document. If set to true, self::bConvertTypes
+         * checks the type of variable and returns the specific field settings
+         * for the cell.
+         * 
+         * @param array $array One-dimensional array with row content
+         */
+        private function addRow ($array)
+        {
+        	$cells = "";
+                foreach ($array as $k => $v):
+                        $type = 'String';
+                        if ($this->bConvertTypes === true && is_numeric($v)):
+                                $type = 'Number';
+                        endif;
+                        $v = htmlentities($v, ENT_COMPAT, $this->sEncoding);
+                        $cells .= "<Cell><Data ss:Type=\"$type\">" . $v . "</Data></Cell>\n"; 
+                endforeach;
+                $this->lines[] = "<Row>\n" . $cells . "</Row>\n";
+        }
 
-            $cells .= "<Cell><Data ss:Type=\"String\">" . utf8_encode($v) . "</Data></Cell>\n"; 
+        /**
+         * Add an array to the document
+         * @param array 2-dimensional array
+         */
+        public function addArray ($array)
+        {
+                foreach ($array as $k => $v)
+                        $this->addRow ($v);
+        }
 
-        endforeach;
 
-        // transform $cells content into one row
-        $this->lines[] = "<Row>\n" . $cells . "</Row>\n";
+        /**
+         * Generate the excel file
+         * @param string $filename Name of excel file to generate (...xls)
+         */
+        public function generateXML ($filename = 'excel-export')
+        {
+                // correct/validate filename
+                $filename = preg_replace('/[^aA-zZ0-9\_\-]/', '', $filename);
+    	
+                // deliver header (as recommended in php manual)
+                header("Content-Type: application/vnd.ms-excel; charset=" . $this->sEncoding);
+                header("Content-Disposition: inline; filename=\"" . $filename . ".xls\"");
 
-    }
+                // print out document to the browser
+                // need to use stripslashes for the damn ">"
+                echo stripslashes (sprintf($this->header, $this->sEncoding));
+                echo "\n<Worksheet ss:Name=\"" . $this->sWorksheetTitle . "\">\n<Table>\n";
+                foreach ($this->lines as $line)
+                        echo $line;
 
-    /**
-     * Add an array to the document
-     * 
-     * This should be the only method needed to generate an excel
-     * document.
-     * 
-     * @access public
-     * @param array 2-dimensional array
-     * @todo Can be transfered to __construct() later on
-     */
-    public function addArray ($array)
-    {
-
-        // run through the array and add them into rows
-        foreach ($array as $k => $v):
-            $this->addRow ($v);
-        endforeach;
-
-    }
-
-    /**
-     * Set the worksheet title
-     * 
-     * Checks the string for not allowed characters (:\/?*),
-     * cuts it to maximum 31 characters and set the title. Damn
-     * why are not-allowed chars nowhere to be found? Windows
-     * help's no help...
-     *
-     * @access public
-     * @param string $title Designed title
-     */
-    public function setWorksheetTitle ($title)
-    {
-
-        // strip out special chars first
-        $title = preg_replace ("/[\\\|:|\/|\?|\*|\[|\]]/", "", $title);
-
-        // now cut it to the allowed length
-        $title = substr ($title, 0, 31);
-
-        // set title
-        $this->worksheet_title = $title;
-
-    }
-
-    /**
-     * Generate the excel file
-     * 
-     * Finally generates the excel file and uses the header() function
-     * to deliver it to the browser.
-     * 
-     * @access public
-     * @param string $filename Name of excel file to generate (...xls)
-     */
-    function generateXML ($filename)
-    {
-
-        // deliver header (as recommended in php manual)
-        header("Content-Type: application/vnd.ms-excel; charset=UTF-8");
-        header("Content-Disposition: inline; filename=\"" . $filename . ".xls\"");
-
-        // print out document to the browser
-        // need to use stripslashes for the damn ">"
-        echo stripslashes ($this->header);
-        echo "\n<Worksheet ss:Name=\"" . $this->worksheet_title . "\">\n<Table>\n";
-        echo "<Column ss:Index=\"1\" ss:AutoFitWidth=\"0\" ss:Width=\"110\"/>\n";
-        echo implode ("\n", $this->lines);
-        echo "</Table>\n</Worksheet>\n";
-        echo $this->footer;
-
-    }
+                echo "</Table>\n</Worksheet>\n";
+                echo $this->footer;
+        }
 
 }
 
