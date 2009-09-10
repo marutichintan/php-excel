2690199f7b11f89d1df321b1d2fd1a4af38b42de
oliver.schwarz <oliver.schwarz@af6d066f-262b-0410-944b-b18571a5ebb2>
2009-09-10 13:56:54 +0000

    Adding a line of documentation.

--- branches/version02/test.php
+++ branches/version02/test.php
@@ -10,6 +10,7 @@ $data = array(
         array('Test', 'Peter')
         );
 
+// generate file (constructor parameters are optional)
 $xls = new Excel_XML('UTF-8', false, 'My Test Sheet');
 $xls->addArray($data);
 $xls->generateXML('my-test');
