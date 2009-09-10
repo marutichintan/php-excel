4dd91c800de86d01a849213821b6615cafb71be6
oliver.schwarz <oliver.schwarz@af6d066f-262b-0410-944b-b18571a5ebb2>
2009-09-10 13:56:19 +0000

    Adding a simple testcase (containing an example).

--- branches/version02/test.php
+++ branches/version02/test.php
@@ -1,4 +1,17 @@
 <?php
 
+// load library
+require 'php-excel.class.php';
+
+// create a simple 2-dimensional array
+$data = array(
+        1 => array ('Name', 'Surname'),
+        array('Schwarz', 'Oliver'),
+        array('Test', 'Peter')
+        );
+
+$xls = new Excel_XML('UTF-8', false, 'My Test Sheet');
+$xls->addArray($data);
+$xls->generateXML('my-test');
 
 ?>
